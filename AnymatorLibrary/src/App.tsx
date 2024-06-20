1b0ba0dfa9ce718f0aa95f5d54dcdf80cd75c6e3
Rutuza <dalal.jyotika05@gmail.com>
2024-06-20 19:57:03 +0530

    typewriter effect done ✅

--- AnymatorLibrary/src/App.tsx
+++ AnymatorLibrary/src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css';
-import Tim from './assets/components/Tim';
+// import Tim from './assets/components/Tim';
+import Typewriter from './assets/components/Typewriter';
 
 function App() {
   return (
     <>
-      <Tim></Tim>
-    
+      {/* <Tim /> */}
+      <Typewriter />
       
     </>
   )
