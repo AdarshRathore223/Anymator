7024576c02162f47fc66387f0e9fc76c2ba45846
Rutuza <dalal.jyotika05@gmail.com>
2024-06-21 16:39:16 +0530

    fade in animation done ✅

--- AnymatorLibrary/src/App.tsx
+++ AnymatorLibrary/src/App.tsx
@@ -1,11 +1,10 @@
 import './App.css';
+import FadeIn from './assets/components/FadeIn';
 // import Bounce from './assets/components/Bounce';
-
 // import Blink from './assets/components/Blink';
-
 // import Tim from './assets/components/Tim';
 // import Typewriter from './assets/components/Typewriter';
-import Rotate from './assets/components/Rotate';
+// import Rotate from './assets/components/Rotate';
 function App() {
   return (
     <>
@@ -13,8 +12,9 @@ function App() {
       {/* <Tim /> */}
       {/* <Typewriter /> */}
       {/* <Blink /> */}
-      <Rotate />
       {/* <Bounce /> */}
+      {/* <Rotate /> */}
+      <FadeIn />
 
     </div>
     </>
--- AnymatorLibrary/tailwind.config.js
+++ AnymatorLibrary/tailwind.config.js
@@ -13,14 +13,24 @@ export default {
           '0%. 100%': {opacity: 1},
           '50%': {opacity: 0},
         },
+
         rotate: {
           '0%': { transform: 'rotate(0deg)' },
           '100%': { transform: 'rotate(360deg)' },
         },
+
+        fadeIn: {
+          '0%': {opacity: 0},
+          '50%': {opacity: 1},
+          '75%': {opacity: 0},
+          '100%': {opacity: 1}
+        },
       },
+      
       animation: {
         blink: 'blink 1s infinite',
         rotate: 'rotate 1s linear infinite',
+        fadeIn: 'fadeIn 2s ease-in-out',
       },
 
     },
