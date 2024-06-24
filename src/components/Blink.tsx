02b9ed4c269e0fd970237bbfc663c187b15a8b5c
Adarsh Rathore <adarsh.rathore@s.amity.edu>
2024-06-24 15:02:17 +0530

    test

--- src/components/Blink.tsx
+++ src/components/Blink.tsx
@@ -5,22 +5,14 @@ interface BlinkProps {
   text: string;
 }
 
-const Blink: React.FC<BlinkProps> = ({ text }:{text:string}) => {
-  const [isVisible, setIsVisible] = useState<boolean>(true);
-
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      setIsVisible((prevVisible) => !prevVisible);
-    }, 500); // Toggle visibility every 500ms
-
-    return () => clearInterval(intervalId);
-  }, []); // Run effect only once on component mount
-
+const Blink = ({ text }: BlinkProps) => {
   return (
-    <p className={`animate-blink absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 font-medium text-5xl text-indigo-500 ${isVisible ? '' : 'invisible'}`}>
+    <p
+      className={`animate-blink absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 font-medium text-5xl text-indigo-500 `}
+    >
       {text}
     </p>
   );
 };
 
-export default Blink;
+export { Blink };
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,3 @@
 export * from "./components/Button"
+export * from "./components/Button2"
+export * from "./components/Blink"
